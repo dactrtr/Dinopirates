bfff7acc8ccc47c0ae332528b2295eb60d1f1fe8
dactrtr <dactrtr@gmail.com>
2022-07-07 23:44:06 +0000

    added enemy test

--- source/entities/enemy.lua
+++ source/entities/enemy.lua
@@ -1,13 +1,15 @@
 
 Enemy = {}
-class('Enemy').extends(NobleSprite)
+class('Enemy').extends(Graphics.sprite)
 
 function Enemy:init(x, y, moveSpeed)
-  Enemy.super.init(self, "assets/images/untitled.png")
+  local enemyImage = Graphics.image.new("assets/images/Untitled.png")
+  self:setImage(enemyImage)
   self:moveTo(x,y)
   self:setCollideRect(0,0, self:getSize())
   self.moveSpeed = moveSpeed
   self:setGroups(1)
+  self:setZIndex(2)
   
   self:add()
 end
@@ -25,5 +27,5 @@ function Enemy:update()
 end
 
 function Enemy:collisionResponse()
-  return "bounce"
+  return "freeze"
 end
