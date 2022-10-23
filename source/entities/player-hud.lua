d9531cb10970a708979a5424bd0b5c522198aa06
dactrtr <dactrtr@gmail.com>
2022-10-23 13:24:00 +0000

    fixed hud init in every scene, comm status func

--- source/entities/player-hud.lua
+++ source/entities/player-hud.lua
@@ -2,29 +2,28 @@ import "entities/player-face/player-eyes"
 import "entities/player-face/player-mouth"
 import "entities/player-face/player-body"
 
-
 class('PlayerHud').extends(Graphics.sprite)
 
-local eyes = PlayerEyes(58,38, status)
-local mouth = PlayerMouth(70,50, status)
-local Body = PlayerBody(60,41, status)
 
-local normal = Graphics.image.new("assets/images/player/hud.png")
+
 
 
 function PlayerHud:init(status)
+  self.status = status
+  local normal = Graphics.image.new("assets/images/player/hud.png")
   self:setImage(normal)
   self:moveTo(56, 40)
-  self.status = status
-
+  local eyes = PlayerEyes(58,38, status)
+  local mouth = PlayerMouth(70,50, status)
+  local Body = PlayerBody(60,41, status)
   self:add()
 end
 
 function PlayerHud:update()
-  if self.status=="dead" then
-    eyes.status = "dead"
-  else
-    eyes.status = "normal"
-  end
+  -- if self.status=="dead" then
+  --   eyes.status = "dead"
+  -- else
+  --   eyes.status = "normal"
+  -- end
 end
 
