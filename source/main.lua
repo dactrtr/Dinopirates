6632b6a1ebb61db571458fbe224999731a72ede7
dactrtr <dactrtr@gmail.com>
2022-10-23 13:25:00 +0000

    comented initial scene

--- source/main.lua
+++ source/main.lua
@@ -3,6 +3,7 @@ import 'libraries/noble/Noble'
 import 'utilities/Utilities'
 
 import 'scenes/InitialScene'
+import 'scenes/SpaceScene'
 
 Noble.Settings.setup({
 	Difficulty = "Medium"
@@ -15,4 +16,5 @@ Noble.GameData.setup({
 Noble.showFPS = true
 playdate.display.setRefreshRate(60)
 
-Noble.new(InitialScene, 1.5, Noble.TransitionType.CROSS_DISSOLVE)
+-- Noble.new(InitialScene, 1.5, Noble.TransitionType.CROSS_DISSOLVE)
+Noble.new(SpaceScene, 1.5, Noble.TransitionType.CROSS_DISSOLVE)
