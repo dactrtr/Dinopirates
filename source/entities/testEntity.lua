0adaa0fe824c817049b49df5a468e9bc8a5e7a72
dactrtr <dactrtr@gmail.com>
2023-06-05 04:28:18 +0000

    cleared test entitty

--- source/entities/testEntity.lua
+++ source/entities/testEntity.lua
@@ -1,23 +1,17 @@
 
 class('TestEntity').extends(Graphics.sprite)
 
+
 function TestEntity:init()
-	print("test initialized")	
+		
 end
 
 function TestEntity:draw()
-	-- print("drawing running")
-	local background = Graphics.image.new(100, 100)
-	Graphics.pushContext(background)
-	Graphics.setColor(Graphics.kColorBlack)
-		Graphics.fillCircleAtPoint(50, 50, 20)
-	Graphics.popContext()
-	local BG = Graphics.sprite.new(background)
-	BG:setZIndex(9)
-	BG:moveTo(200, 140)
-	BG:add()
-		
+	
 end
 function TestEntity:update()
+
+end
+function TestEntity:clear()
 
 end
