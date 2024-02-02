e46464bf631ee151a127046b039eebbd36629ab0
dactrtr <dactrtr@gmail.com>
2024-02-02 10:03:41 -0300

    map fixed

--- source/entities/UI/map.lua
+++ source/entities/UI/map.lua
@@ -1,45 +1,25 @@
-Map = {}
-class('Map').extends(Graphics.sprite)
+MapBG = {}
+class('MapBG').extends(Graphics.sprite)
 
 local maze <const> = Graphics.image.new("assets/images/ui/map.png")
+local fog <const> = Graphics.image.new(maze:getSize())
 
-local rooms = {
-    { visited = true },
-    { visited = false },
-    { visited = true },
-    { visited = true },
-    { visited = false },
-    { visited = false },
-    { visited = true },
-    { visited = true },
-    { visited = false }
-}
-function Map:init()
+function MapBG:init()
     self:setImage(maze)
     self:setZIndex(ZIndex.ui)
     self:moveTo(16,34)
-    
-    local alpha = 0.5
-    local roomSize = 7
-    local row = 0
-    local col = 0
-        
-                   
-     for i = 1, 9 do 
-         if rooms[i].visited == true then 
-             
-             col = (i - 1) % 3
-             row = math.floor((i - 1) / 3)
-             
-             Graphics.pushContext(maze)
-             
-             Graphics.setColor(Graphics.kColorBlack)
-             Graphics.setDitherPattern(alpha, Graphics.image.kDitherTypeBayer8x8)
-             Graphics.fillRect(4 + (col * 6), 4 + (row * 6), roomSize, roomSize)
-             
-             Graphics.popContext() 
-         end
-     end
+    self:add()
+end
+
+Map = {}
+
+class('Map').extends(Graphics.sprite)
+
+function Map:init()
+    self:setImage(fog)
+    self:setZIndex(ZIndex.ui+1)
+    self:moveTo(16,34)
+    mapbg = MapBG()
        
     --    Graphics.pushContext(maze)
     -- 
@@ -60,6 +40,27 @@ function Map:init()
 end
 
 function Map:update()
+    local alpha = 0.5
+    local roomSize = 7
+    local row = 0
+    local col = 0
+    fog:clear(Graphics.kColorClear)
+                   
+     for i = 1, 9 do 
+         if rooms[i].visited == false then 
+             
+             col = (i - 1) % 3
+             row = math.floor((i - 1) / 3)
+             
+             Graphics.pushContext(fog)
+             
+             Graphics.setColor(Graphics.kColorBlack)
+             Graphics.setDitherPattern(alpha, Graphics.image.kDitherTypeBayer8x8)
+             Graphics.fillRect(4 + (col * 6), 4 + (row * 6), roomSize, roomSize)
+             
+             Graphics.popContext() 
+         end
+     end
 end
 
 
